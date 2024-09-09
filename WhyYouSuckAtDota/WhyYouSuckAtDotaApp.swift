fbf4f010346e816267e1120d53dd0de18d3a451f
Caleb Voorhees <cvoorhees44@gmail.com>
2024-09-08 20:38:43 -0400

    Initial Commit

---
+++ WhyYouSuckAtDota/WhyYouSuckAtDotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyYouSuckAtDotaApp.swift
+//  WhyYouSuckAtDota
+//
+//  Created by Caleb Voorhees on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyYouSuckAtDotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
