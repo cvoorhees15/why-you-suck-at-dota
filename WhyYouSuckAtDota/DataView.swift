ef8cd0433b24d64ea9cf6e650a7d8017aaa99eba
Caleb Voorhees <cvoorhees44@gmail.com>
2024-09-12 22:56:36 -0400

    Additions to OpenDotaService
    - Added structs for Dota match data
    
    - Added functions to fetch that data
    
    - Listed that data in the DataView

--- WhyYouSuckAtDota/DataView.swift
+++ WhyYouSuckAtDota/DataView.swift
@@ -10,9 +10,32 @@ import SwiftUI
 struct DataView: View {
     
     @State var account_ID: Int
+    @State var matches: [OpenDotaService.Match] = []
+    @State var recentmatches: [OpenDotaService.RecentMatch] = []
+    var ODS = OpenDotaService()
     
     var body: some View {
-        Text(String(account_ID))
+        List(recentmatches, id: \.match_id) {
+            recentmatch in
+            Text(String(recentmatch.match_id))
+        }
+        .task {
+            do {
+                recentmatches = try await ODS.fetchRecentMatches(accountId: account_ID)
+            }
+            catch OpenDotaService.ApiError.invalidURL {
+                print ("invalid URL")
+            }
+            catch OpenDotaService.ApiError.invalidReponse {
+                print ("invalid response")
+            }
+            catch OpenDotaService.ApiError.invalidData {
+                print ("invalid data")
+            }
+            catch {
+                print ("unexpected error")
+            }
+        }
     }
 }
 
--- WhyYouSuckAtDota/OpenDotaService.swift
+++ WhyYouSuckAtDota/OpenDotaService.swift
@@ -27,11 +27,66 @@ class OpenDotaService
         let account_id: Int
     }
     
-    struct SearchResult: Codable 
+    struct Player: Codable
+    {
+        let hero_id: Int
+        let item_0: Int
+        let item_1: Int
+        let item_2: Int
+        let item_3: Int
+        let item_4: Int
+        let item_5: Int
+        let item_6: Int
+        let backpack_0: Int
+        let backpack_1: Int
+        let backpack_2: Int
+        let kills: Int
+        let deaths: Int
+        let assists: Int
+        let last_hits: Int
+        let denies: Int
+        let gold_per_min: Int
+        let xp_per_min: Int
+        let level: Int
+        let net_worth: Int
+        let aghanims_scepter: Int? //Bool
+        let aghanims_shard: Int? //Bool
+        let hero_damage: Int
+        let tower_damage: Int
+        let hero_healing: Int
+        let ability_upgrades_arr: [Int]
+        let duration: Int
+        let win: Int //Bool
+        let lose: Int //Bool
+        let total_gold: Int
+        let total_xp: Int
+        let kills_per_min: Float
+        let kda: Int
+    }
+    
+    struct SearchResult: Codable
     {
         let account_id: Int
         let personaname: String
         let avatarfull: String
+    }
+    
+    struct Match: Codable
+    {
+        let players: [Player]
+        let first_bood_time: Int
+    }
+    
+    struct RecentMatch: Codable
+    {
+        let match_id: Int
+    }
+    
+    struct ProMatch: Codable
+    {
+        let match_id: Int
+        let radiant_team: [Int]
+        let dire_team: [Int]
     }
     // *****************************************
     
@@ -74,6 +129,46 @@ class OpenDotaService
         do {
             let decoder = JSONDecoder()
             return try decoder.decode([SearchResult].self, from: data)
+        } catch {
+            throw ApiError.invalidData
+        }
+    }
+    
+    func fetchMatch(matchId: Int) async throws -> Match
+    {
+        let data = try await openDotaAPICall(endpoint: "https://api.opendota.com/api/matches/\(matchId)")
+        
+        // Translate JSON response from API call
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(Match.self, from: data)
+        } catch {
+            throw ApiError.invalidData
+        }
+    }
+    
+    func fetchRecentMatches(accountId: Int) async throws -> [RecentMatch]
+    {
+        let data = try await openDotaAPICall(endpoint: "https://api.opendota.com/api/players/\(accountId)/recentMatches")
+        
+        // Translate JSON response from API call
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([RecentMatch].self, from: data)
+        } catch {
+            throw ApiError.invalidData
+        }
+
+    }
+    
+    func fetchProPubMatches() async throws -> [ProMatch]
+    {
+        let data = try await openDotaAPICall(endpoint: "https://api.opendota.com/api/publicMatches/?min_rank=81")
+        
+        // Translate JSON response from API call
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([ProMatch].self, from: data)
         } catch {
             throw ApiError.invalidData
         }
